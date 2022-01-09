5951458b22633dc7701b9494ccb623cd91685698
zawadzamil <contact.zawadzamil@gmail.com>
2022-01-10 00:03:26 +0600

    second commit

--- js/app.js
+++ js/app.js
@@ -23,7 +23,8 @@ let a;
 let time;
 setInterval(() => {
   a = new Date();
-  time = a.getHours() + ':' + a.getMinutes() + ':' + a.getSeconds();
+  let time = a.toLocaleTimeString();
+ // time = a.getHours() + ':' + a.getMinutes() + ':' + a.getSeconds();
   document.getElementById('time').innerHTML = time;
 }, 1000);
 
